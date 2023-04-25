c3ec0c628a77f6e8ea6d423e99a0678f914ebcbd
l3miage-mesouaks <salaheddin.mesouak@etu.univ-grenoble-alpes.fr>
2023-04-25 16:42:02 +0200

    "DTO"

---
+++ rest-api/src/main/java/fr/uga/l3miage/example/response/PersonneDTO.java
@@ -0,0 +1,26 @@
+package fr.uga.l3miage.example.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Schema(description = "correspond au DTO de l'entité Personne")
+public class PersonneDTO {
+
+    @Schema(description = "correspond à l'Id de la personne",example = "1")
+    Long id;
+
+    @Schema(description = "correspond au nom de la personne",example = "Salaheddin")
+    String nom;
+
+    @Schema(description = "correspond au login de la personne",example = "mesouaks")
+    String login;
+
+    @Schema(description = "correspond au password de la personne",example = "123456789")
+    String password;
+
+
+
+}
---
+++ server/src/main/java/fr/uga/l3miage/example/service/PersonneService.java
@@ -0,0 +1,4 @@
+package fr.uga.l3miage.example.service;
+
+public class PersonneService {
+}
