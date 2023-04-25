17625e01044bd2a36826ba701e6e591c6992eca3
Kyllian <charre.kyllian@gmail.com>
2023-04-25 15:56:00 +0200

    git ignore

--- front-project/src/app/app.module.ts
+++ front-project/src/app/app.module.ts
@@ -1,23 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-// import { ComponentQuestionComponent } from './component-question/component-question.component';
-// import { ComponentReponseComponent } from './component-reponse/component-reponse.component';
+//import { ComponentQuestionComponent } from './component-question/component-question.component';
+//import { ComponentReponseComponent } from './component-reponse/component-reponse.component';
 import { ReponseComponent } from './reponse/reponse.component';
-// import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
     // ComponentQuestionComponent,
     // ComponentReponseComponent,
-    ReponseComponent
-    // MatToolbarModule
+     
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
     AppRoutingModule
   ],
   providers: [],
--- front-project/src/app/question/question.component.ts
+++ front-project/src/app/question/question.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css']
+  styleUrls: ['./question.component.css'],
+  standalone: true,
 })
 
 export class QuestionComponent {
