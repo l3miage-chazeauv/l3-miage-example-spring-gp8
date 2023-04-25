2b452ac0ab342d80a57f44e6294554c4b1da7231
KANTHEON <quentin.bebin@etu.univ-grenoble-alpes.fr>
2023-04-25 13:12:38 +0200

    front app-reponse

--- front-project/src/app/app.module.ts
+++ front-project/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentQuestionComponent } from './component-question/component-question.component';
+import { ComponentReponseComponent } from './component-reponse/component-reponse.component';
+import { ReponseComponent } from './reponse/reponse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentQuestionComponent,
+    ComponentReponseComponent,
+    ReponseComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-project/src/app/reponse/reponse.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reponse',
+  templateUrl: './reponse.component.html',
+  styleUrls: ['./reponse.component.css']
+})
+export class ReponseComponent {
+
+  private label:String="intitulé de la réponse";
+  private estValide:Boolean=false;
+  
+}
