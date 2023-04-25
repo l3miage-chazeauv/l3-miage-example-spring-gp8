d2e308560058b7b0c5b6ae73935fa7293d208c55
Chazeau Vincent <vincent.chazeau@etu.univ-grenoble-alpes.fr>
2023-04-25 16:16:49 +0200

    Config toolbar

--- front-project/src/app/app.module.ts
+++ front-project/src/app/app.module.ts
@@ -1,19 +1,15 @@
 
 import { AppRoutingModule } from './app-routing.module';
-//import { ComponentQuestionComponent } from './component-question/component-question.component';
-//import { ComponentReponseComponent } from './component-reponse/component-reponse.component';
-import { ReponseComponent } from './reponse/reponse.component';
-
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppComponent } from './app.component';
 // import { ComponentQuestionComponent } from './component-question/component-question.component';
 // import { ComponentReponseComponent } from './component-reponse/component-reponse.component';
 import { ReponseComponent } from './reponse/reponse.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -25,7 +21,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
