e577748ddbd9340ca64646ed32b84cd5652d6554
Chazeau Vincent <vincent.chazeau@etu.univ-grenoble-alpes.fr>
2023-04-25 14:08:37 +0200

    Push AppComponent 1

--- front-project/src/app/app.module.ts
+++ front-project/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentQuestionComponent } from './component-question/component-question.component';
-import { ComponentReponseComponent } from './component-reponse/component-reponse.component';
+// import { ComponentQuestionComponent } from './component-question/component-question.component';
+// import { ComponentReponseComponent } from './component-reponse/component-reponse.component';
 import { ReponseComponent } from './reponse/reponse.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentQuestionComponent,
-    ComponentReponseComponent,
-    ReponseComponent
+    // ComponentQuestionComponent,
+    // ComponentReponseComponent,
+    ReponseComponent,
+    MatToolbarModule
   ],
   imports: [
     BrowserModule,
