a3f5f40168c9e5ead165b6458eb29ede7e343d86
Chazeau Vincent <vincent.chazeau@etu.univ-grenoble-alpes.fr>
2023-04-26 13:49:35 +0200

    Ajout firebase

--- front-project/src/app/app.component.ts
+++ front-project/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Auth, authState, GoogleAuthProvider, signInWithPopup, signOut, User } from '@angular/fire/auth';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { QcmService } from './qcm.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,28 @@ import { Auth, authState, GoogleAuthProvider, signInWithPopup, signOut, User } f
 export class AppComponent {
   title = 'Miahoot';
 
+  bsAuth = new BehaviorSubject<boolean>(false); // état de la connection
+  public readonly user: Observable<User | null>; // utilisateur connecté
+
+  constructor(private auth: Auth, private qs: QcmService) {
+    this.user = authState(auth); // récupération de l'utilisateur connecté
+  }
+
+  async login() {
+    this.bsAuth.next(true); // on passe l'état de la connection à true
+    const googleProvider = new GoogleAuthProvider(); // on utilise le provider Google
+
+    googleProvider.setCustomParameters({ // on demande à l'utilisateur de choisir son compte
+      prompt: 'select_account'
+    });
+
+    try{
+      await signInWithPopup(this.auth, googleProvider); // on ouvre une popup pour se connecter
+    } catch(e){
+      console.log("Login error (Google): " + e); // si erreur, on affiche l'erreur de login
+    }
+
+    this.bsAuth.next(false); // on passe l'état de la connection à false
+  }
 
 }
-
-
