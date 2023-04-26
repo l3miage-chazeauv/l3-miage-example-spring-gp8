035b6310b6589d0d5141ab42a513c576f0858abe
KANTHEON <quentin.bebin@etu.univ-grenoble-alpes.fr>
2023-04-26 16:12:59 +0200

    cac

--- front-project/src/environments/environment.ts
+++ front-project/src/environments/environment.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'projetintmiahoot',
+    appId: '1:845241807448:web:7b3d127dfab47640f8d02d',
+    storageBucket: 'projetintmiahoot.appspot.com',
+    apiKey: 'AIzaSyCGc9BPhg_nt0WJpbv0BhAwU3Wbpn1qo08',
+    authDomain: 'projetintmiahoot.firebaseapp.com',
+    messagingSenderId: '845241807448',
+  },};
