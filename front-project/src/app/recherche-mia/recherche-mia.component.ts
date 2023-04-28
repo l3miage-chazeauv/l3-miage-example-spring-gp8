3f755857b519606a20645b485b67480377c8ac34
Kyllian <charre.kyllian@gmail.com>
2023-04-28 11:00:42 +0200

    ok

--- front-project/src/app/recherche-mia/recherche-mia.component.ts
+++ front-project/src/app/recherche-mia/recherche-mia.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { APIService } from '../api.service';
+import { RoutingService } from '../routing.service';
 
 @Component({
   selector: 'app-recherche-mia',
@@ -10,14 +11,16 @@ export class RechercheMiaComponent {
 
   public idRecherche?:number;
 
-  constructor(private apiMia : APIService) { }
+  constructor(private apiMia : APIService,protected router: RoutingService) { }
   /* Fonction de recherche parID qui interogera la BD et si le miahoot existe nous dirige sur la waitingRoom de ce miahoot
   searchMiahootById(id:string):void*/
 
   findMiahootById():void{
-    this.apiMia.getAPIById('miahoot/' + this.idRecherche).subscribe((data:any)=>{
+    this.apiMia.getAPI('miahoot/' + this.idRecherche).subscribe((data:any)=>{
       console.log(data)
     
     })
+    
+
   }
 }
