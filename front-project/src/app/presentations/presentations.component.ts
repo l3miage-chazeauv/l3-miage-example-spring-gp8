c12087d8db6502a2a27a9b756153b0d81287a20b
Chazeau Vincent <vincent.chazeau@etu.univ-grenoble-alpes.fr>
2023-05-04 17:28:22 +0200

    ok i push up

--- front-project/src/app/presentations/presentations.component.ts
+++ front-project/src/app/presentations/presentations.component.ts
@@ -19,20 +19,51 @@ export class PresentationsComponent {
   constructor(private auth: Auth, private apiMia: APIService) {
     this.user = authState(this.auth); // récupération de l'utilisateur connecté
     
-    this.user.subscribe( u => {
+    // this.user.subscribe( u => {
       
-      if(u != null){
-        this.idUserFB = u.uid; // récupération de l'id de l'utilisateur connecté
-        console.log("iduser: " + this.idUserFB);
+    //   if(u != null){
+    //     this.idUserFB = u.uid; // récupération de l'id de l'utilisateur connecté
+    //   }
+
+    //   this.apiMia.getAPIMmiahootsPresented(this.idUserFB).subscribe( (data: any) => {
+    //     this.listePresentations = data;
+    //   });
+
+    // });
+
+    const userPromise = new Promise<User | null>((resolve, reject) => {
+      this.user.subscribe(
+        u => {
+          if (u != null) {
+            this.idUserFB = u.uid;
+          }
+          resolve(u);
+        },
+        error => reject(error)
+      );
+    });
+    
+    const apiPromise = userPromise.then(user => {
+      if (user != null) {
+        return this.apiMia.getAPIMmiahootsPresented(this.idUserFB).toPromise();
+      } else {
+        return null;
       }
-
-      this.apiMia.getAPIMmiahootsPresented(this.idUserFB).subscribe( (data: any) => {
-        this.listePresentations = data;
-      });
-
-      console.log(this.listePresentations);
     });
 
+    Promise.all([userPromise, apiPromise]).then(([user, apiData]) => {
+      if (apiData != null) {
+        // this.listePresentations = apiData;
+        console.log("api: " + apiData.toString());
+        console.log(user?.uid);
+        // le reste du code qui utilise les nouvelles valeurs de idUserFB et listePresentations
+      }
+    }).catch(error => {
+      console.log(error);
+    });
+
+    // console.log("user: " + userPromise.all.tostring());
+    // console.log("listePresentations: " + apiPromise.tostring());
   }
 
 }
